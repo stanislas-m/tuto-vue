e946f1063d470ef397f4e7fcbd6f9585f3a3f31a
Stanislas Michalak <smichalak@molotov.tv>
2022-03-30 11:04:50 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} {{ msg2 }}</h1>
+    <p>Nombre d'utilisateurs : {{ users.length }}</p>
+    <button @click.prevent="onBtnClicked">
+      Clique pour ajouter
+    </button>
+    <div v-for="(user, index) in users" :key="index" class="user-container">
+      <button @click="onDelete(index)">
+        Clique pour supprimer
+      </button>
+      User {{ user.name }} : {{ user.age }}
+    </div>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    msg2: String,
+  },
+  data: function () {
+    return {
+      users: [{
+        name: 'Toto',
+        age: 10
+      }]
+    }
+  },
+  methods: {
+    onDelete: function (index) {
+      this.users.splice(index, 1)
+    },
+    onBtnClicked: function () {
+      this.users.push({
+        name: 'Toto ' + this.users.length,
+        age: this.users.length
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.user-container {
+  background: #dfdfdf;
+  border: solid 1px #333;
+}
+</style>
